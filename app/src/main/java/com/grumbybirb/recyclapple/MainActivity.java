7b73684ca3bf4f587c52d1856bf4c2275389fef9
Joe <joe.stephenson36@gmail.com>
2017-02-27 19:04:13 +0000

    barcode working

--- app/src/main/java/com/grumbybirb/recyclapple/MainActivity.java
+++ app/src/main/java/com/grumbybirb/recyclapple/MainActivity.java
@@ -1,9 +1,6 @@
 package com.grumbybirb.recyclapple;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -12,10 +9,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (null == savedInstanceState) {
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.container, Camera2BasicFragment.newInstance())
-                    .commit();
-        }
+//        if (null == savedInstanceState) {
+//            getFragmentManager().beginTransaction()
+//                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+//                    .commit();
+//        }
     }
 }
