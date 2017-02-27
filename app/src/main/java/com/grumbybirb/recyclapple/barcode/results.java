1c93c388dcf81308fd9bbab2354506552a404ee9
Joe <joe.stephenson36@gmail.com>
2017-02-27 23:58:26 +0000

    second view started

---
+++ app/src/main/java/com/grumbybirb/recyclapple/barcode/results.java
@@ -0,0 +1,18 @@
+package com.grumbybirb.recyclapple.barcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.grumbybirb.recyclapple.R;
+
+public class results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+
+
+
+}
