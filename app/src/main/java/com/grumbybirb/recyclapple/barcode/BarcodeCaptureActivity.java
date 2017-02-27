a2ba6b4b1273b8a00fde55cc1c2d34008894e3fd
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-02-27 20:25:21 +0000

    Add toggle flash button

--- app/src/main/java/com/grumbybirb/recyclapple/barcode/BarcodeCaptureActivity.java
+++ app/src/main/java/com/grumbybirb/recyclapple/barcode/BarcodeCaptureActivity.java
@@ -91,14 +91,13 @@ public final class BarcodeCaptureActivity extends AppCompatActivity {
         mGraphicOverlay = (GraphicOverlay<BarcodeGraphic>) findViewById(R.id.graphicOverlay);
 
         // read parameters from the intent used to launch the activity.
-        boolean autoFocus = getIntent().getBooleanExtra(AutoFocus, true);
         boolean useFlash = getIntent().getBooleanExtra(UseFlash, true);
 
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         if (rc == PackageManager.PERMISSION_GRANTED) {
-            createCameraSource(autoFocus, useFlash);
+            createCameraSource(true, useFlash);
         } else {
             requestCameraPermission();
         }
