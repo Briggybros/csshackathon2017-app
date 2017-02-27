7a7b51e3d3fd825e9eba01de9d51c18769bbef3c
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-02-27 15:24:15 +0000

    tried adding camera

--- app/src/main/java/com/grumbybirb/recyclapple/MainActivity.java
+++ app/src/main/java/com/grumbybirb/recyclapple/MainActivity.java
@@ -1,13 +1,21 @@
 package com.grumbybirb.recyclapple;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
     }
 }
