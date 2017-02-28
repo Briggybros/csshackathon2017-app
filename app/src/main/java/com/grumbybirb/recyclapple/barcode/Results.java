4332af6a3c9f56058acc58fcc50723308d36f690
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-02-28 01:06:05 +0000

    Update Results.java

--- app/src/main/java/com/grumbybirb/recyclapple/barcode/Results.java
+++ app/src/main/java/com/grumbybirb/recyclapple/barcode/Results.java
@@ -1,15 +1,44 @@
 package com.grumbybirb.recyclapple.barcode;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import com.grumbybirb.recyclapple.R;
+
 import java.util.List;
 
-/**
- * Created by bkazi on 27/02/2017.
- */
+public class Results extends AppCompatActivity {
 
-public class Results {
-    private List<Instruction> results;
+    private InstructionsAdapter instructionsAdapter;
 
-    public List<Instruction> getResults() {
-        return results;
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent openingIntent = getIntent();
+        String barcode = openingIntent.getStringExtra("barcode");
+
+        Location location = GetLocation.getLocation(this);
+
+        InstructionPopulator instructionPopulator = new InstructionPopulator(this, instructionsAdapter);
+        instructionPopulator.fetchInstructions(barcode, location);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        GetLocation.requestLocPerms(this);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        instructionsAdapter = new InstructionsAdapter(this);
+        listView.setAdapter(instructionsAdapter);
+    }
+
 }
